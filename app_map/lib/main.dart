de0d511add1d37d41b0bce5120eec0dc292375cc
divyesh26 <divyesh.r@solutelabs.com>
2021-02-20 10:39:44 +0530

    setup ui

--- app_map/lib/main.dart
+++ app_map/lib/main.dart
@@ -1,5 +1,36 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MyPage());
+}
+
+class MyPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Map'),
+      ),
+      body: Container(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.location_searching),
+      ),
+    );
+  }
 }
